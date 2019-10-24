603e717c574b6f2746b4538912017edcff381a63
Larry <larrydag@sbcglobal.net>
2019-10-23 22:30:21 -0500

    add file via upload

---
+++ horror_movies.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+horror_movies <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-22/horror_movies.csv")
+hm <- horror_movies
+
+head(hm[,1:6])
+head(hm[,7:12])
+
+### modify variables
+hm$movie_run_time_num <- as.numeric(gsub(" min","", hm$movie_run_time))
+hm$budget_num <- gsub("[[:punct:]]","", hm$budget)
+hm$budget_num <- gsub("INR","", hm$budget_num)
+hm$budget_num <- as.numeric(hm$budget_num)
+hm$log_budget_num <- log(hm$budget_num)
+hm$budget_unit <- substr(hm$budget,1,1)
+
+
+#### budget vs rating #####
+hmtemp <- hm[which(hm$budget_unit=="$" & hm$review_rating>0),]
+head(hmtemp[,c("budget_num","review_rating")])
+
+ggplot(hmtemp, aes(x=review_rating, y=log_budget_num)) +
+  geom_point() +
+  geom_smooth(method='lm',formula=y~x) +
+  ggtitle("Tidy Tuesday Horror Movie Ratings vs Budget") +
+  ylab("log(Budget)") +
+  xlab("Movie Rating")
